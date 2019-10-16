fba05ba1d8a0bb467613bd3c6f4892e50c765037
GrigorVlad <Grigsogood96@gmail.com>
2019-10-16 10:11:31 +0300

    Optimize imports, delete comments and add "private" on fields in benchmark

--- src/main/java/me/grigor/benchmark/ForBenchmark.java
+++ src/main/java/me/grigor/benchmark/ForBenchmark.java
@@ -6,10 +6,8 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
-import org.openjdk.jmh.runner.options.TimeValue;
 
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
 
 @OutputTimeUnit(TimeUnit.SECONDS)
 @Warmup(iterations = 5, time = 1)
@@ -18,39 +16,39 @@ import java.util.concurrent.locks.Lock;
 @State(Scope.Benchmark)
 public class ForBenchmark {
 
-    Counter concurrentCounter = new ConcurrentCounter();
-    Counter lockCounter = new LockCounter();
-    Counter mutexCounter = new MutexCounter();
-    Counter magicCounter = new MagicCounter();
+    private Counter concurrentCounter = new ConcurrentCounter();
+    private Counter lockCounter = new LockCounter();
+    private Counter mutexCounter = new MutexCounter();
+    private Counter magicCounter = new MagicCounter();
     private static final int n = 2;
 
-//    @Benchmark
-//    @Group("ConcurrentCounter")
-//    @GroupThreads(n)
-//    public void checkConcurrentIncrement() {
-//        concurrentCounter.increment();
-//    }
-//
-//    @Benchmark
-//    @Group("ConcurrentCounter")
-//    @GroupThreads(n)
-//    public long checkConcurrentGet() {
-//        return concurrentCounter.getValue();
-//    }
-//
-//    @Benchmark
-//    @Group("LockCounter")
-//    @GroupThreads(n)
-//    public void checkLockIncrement() {
-//        lockCounter.increment();
-//    }
-//
-//    @Benchmark
-//    @Group("LockCounter")
-//    @GroupThreads(n)
-//    public long checkLockGet() {
-//        return lockCounter.getValue();
-//    }
+    @Benchmark
+    @Group("ConcurrentCounter")
+    @GroupThreads(n)
+    public void checkConcurrentIncrement() {
+        concurrentCounter.increment();
+    }
+
+    @Benchmark
+    @Group("ConcurrentCounter")
+    @GroupThreads(n)
+    public long checkConcurrentGet() {
+        return concurrentCounter.getValue();
+    }
+
+    @Benchmark
+    @Group("LockCounter")
+    @GroupThreads(n)
+    public void checkLockIncrement() {
+        lockCounter.increment();
+    }
+
+    @Benchmark
+    @Group("LockCounter")
+    @GroupThreads(n)
+    public long checkLockGet() {
+        return lockCounter.getValue();
+    }
 
     @Benchmark
     @Group("MutexCounter")
@@ -66,19 +64,19 @@ public class ForBenchmark {
         return mutexCounter.getValue();
     }
 
-//    @Benchmark
-//    @Group("MagicCounter")
-//    @GroupThreads(n)
-//    public void checkMagicIncrement() {
-//        magicCounter.increment();
-//    }
-//
-//    @Benchmark
-//    @Group("MagicCounter")
-//    @GroupThreads(n)
-//    public long checkMagicGet() {
-//        return magicCounter.getValue();
-//    }
+    @Benchmark
+    @Group("MagicCounter")
+    @GroupThreads(n)
+    public void checkMagicIncrement() {
+        magicCounter.increment();
+    }
+
+    @Benchmark
+    @Group("MagicCounter")
+    @GroupThreads(n)
+    public long checkMagicGet() {
+        return magicCounter.getValue();
+    }
 
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
---
+++ target/generated-sources/annotations/me/grigor/benchmark/generated/ForBenchmark_jmhType.java
@@ -0,0 +1,4 @@
+package me.grigor.benchmark.generated;
+public class ForBenchmark_jmhType extends ForBenchmark_jmhType_B3 {
+}
+
